1e84a9f2343d7c4f3216e0a4437ff2ee10c2e487
Erik Dubois <erik.dubois@gmail.com>
2023-07-17 10:07:26 +0200

    candy fase 2

--- install-candy-icons.sh
+++ install-candy-icons.sh
@@ -13,6 +13,9 @@
 
 git clone https://github.com/arcolinux/a-candy-beauty-icon-theme.git /tmp/candy
 
+mkdir $HOME/.icons
+mv /tmp/candy/usr/share/icons/* $HOME/.icons/
+
 echo "################################################################"
 echo "###################    zsh installed     ######################"
 echo "################################################################"
