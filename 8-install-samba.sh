a2f902ae58af4e589f48d54a0677cd4a98c78210
Erik Dubois <erik.dubois@gmail.com>
2023-07-21 11:36:14 +0200

    update

--- 8-install-samba.sh
+++ 8-install-samba.sh
@@ -11,7 +11,16 @@
 #
 ##################################################################################################################
 
+# not working
+
 sudo apt install -y samba
+
+sudo mv /etc/samba/smb.conf /etc/samba/samba.conf.bak
+sudo wget https://raw.githubusercontent.com/arcolinux/arcolinux-system-config/master/etc/samba/smb.conf.arcolinux -O /etc/samba/smb.conf
+
+echo "remove the semi-colons of the last lines"
+
+mkdir $HOME/SHARED
 
 echo "################################################################"
 echo "###################    samba installed     ######################"
